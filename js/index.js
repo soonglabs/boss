4850a0d9cf1032fad786124ee76e63c8ea5be26f
Scott Russell <scrussell24@gmail.com>
2016-07-12 08:32:26 -0500

    added closure

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-var root;
-if(localStorage['fs.main']){
-    root = JSON.parse(localStorage['fs.main']);
-} else{
-    root = JSON_IMAGE;
-}
+($(document).ready(function() {
+    //start doing stuff!!
+    var root;
+    if(localStorage['fs.main']){
+        root = JSON.parse(localStorage['fs.main']);
+    } else{
+        root = JSON_IMAGE;
+    }
 
-var main_boss = new Boss(root, 'main');
-var client = new BossTermClient(main_boss);
+    var main_boss = new Boss(root, 'main');
+    var client = new BossTermClient(main_boss);
 
-$(document).ready(function() {
     $('#terminal').terminal(client.exec, {
         greetings: '',
         name: 'boss',
         prompt: 'username: '
     });
-});
+}));
