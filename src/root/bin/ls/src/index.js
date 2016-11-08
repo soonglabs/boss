720e5e2cb6685c315e6de6b92880c8210e4e2fc1
Scott Russell <scrussell24@gmail.com>
2016-11-08 17:25:36 -0600

    remove some new lines

--- src/root/bin/ls/src/index.js
+++ src/root/bin/ls/src/index.js
@@ -1,11 +1,11 @@
 (function(args, client){
+    'use strict';
     var str = '';
     var path = boss.lib.utils.calcAbsPath(args[1],args);
     var list = boss.fs.get_contents(path);
-    
-    for( var i in list ){
+    for(var i in list){
         str += list[i] + '\n';
     }
-    
+    str.slice(0,-2);
     boss.lib.print.log(String(str), client);
 })
