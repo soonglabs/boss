08c37958e35cf78f5ddbcedf108cdad3c79fcc4d
Scott Russell <scrussell24@gmail.com>
2016-11-11 18:46:37 -0600

    update prompt

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,6 @@
     $('#terminal').terminal(client.exec, {
         greetings: '',
         name: 'boss',
-        prompt: name + '$ username:'
+        prompt: name + ': username$ '
     });
 }));
--- src/root/lib/Login/src/index.js
+++ src/root/lib/Login/src/index.js
@@ -7,7 +7,7 @@
         if(!boss.fs.get_user(command)) {
             boss.lib.print.error('unknown user', client);
         } else if(boss.fs.get_user(command).password){
-            boss.lib.set_prompt('password:', client);
+            boss.lib.set_prompt('password', client);
             username = command;
             boss.lib.push(this.password);
         }
@@ -17,7 +17,7 @@
         
         if(boss.fs.get_user(username).password == boss.lib.utils.hashCode(command)){
             boss.fs.set_current_user(username);
-            boss.lib.set_prompt( username + '>', client);
+            boss.lib.set_prompt( username, client);
             boss.fs.set_cwd('/home/' + username);
             boss.lib.pop();
             boss.lib.push(new boss.lib.Shell(boss).exec);
@@ -27,7 +27,7 @@
             boss.lib.print.log('Hello ' + username + '. Welcome to BOSS. Type [[;orange;]help] to see available commands.', client);
         } else {
             boss.lib.print.error('unknown username/password combination', client);
-            boss.lib.set_prompt('username:', client);
+            boss.lib.set_prompt('username', client);
             boss.lib.pop();
         }
     }
