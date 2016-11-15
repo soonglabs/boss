b15fe47807e492efa1e06c501d0506e4ed1e6017
Scott Russell <scrussell24@gmail.com>
2016-11-14 19:33:26 -0600

    removed comment

--- src/root/lib/Login/src/index.js
+++ src/root/lib/Login/src/index.js
@@ -1,12 +1,11 @@
 (function(boss){
-
+    'use strict';
     var username = false;
 
     this.username = (command, client) => {
-
-            boss.lib.set_prompt('password', client);
-            username = command;
-            boss.lib.push(this.password);
+        boss.lib.set_prompt('password', client);
+        username = command;
+        boss.lib.push(this.password);
     };
 
     this.password = (command, client) => {
@@ -25,19 +24,5 @@
             boss.lib.set_prompt('username', client);
             boss.lib.pop();
         }
-
-        // if(boss.fs.get_user(username).password == boss.lib.utils.hashCode(command)){
-        //     boss.fs.set_current_user(username);
-        //     boss.lib.set_prompt( username, client);
-        //     boss.fs.set_cwd('/home/' + username);
-        //     boss.lib.pop();
-        //     boss.lib.push(new boss.lib.Shell(boss).exec);
-        //     boss.cmd.about('', client);
-        //     boss.lib.print.log('Hello ' + username + '. Welcome to BOSS. Type [[;orange;]help] to see available commands.', client);
-        // } else {
-        //     boss.lib.print.error('unknown username/password combination', client);
-        //     boss.lib.set_prompt('username', client);
-        //     boss.lib.pop();
-        // }
     };
 });
