4b0f21199630d331198ef5065f7ceec491539bfe
Scott Russell <scrussell24@gmail.com>
2016-11-14 20:35:49 -0600

    refactoring shell app

--- src/root/app/shell/src/index.js
+++ src/root/app/shell/src/index.js
@@ -1,10 +1,30 @@
-(function(args){
+(function(){
     'use strict';
 
     var client = new boss.lib.TerminalClient(boss);
     var template = '<div class="shell"></div>';
 
-    var vm = new boss.lib.VueApp(template, {
+    var layoutConfig = {
+        content: [{
+            type: 'row',
+            content: [
+                {
+                    type: 'component',
+                    componentName: 'shell',
+                    componentState: {}
+                }
+            ]
+        }]
+    };
+
+    var layout = new GoldenLayout(layoutConfig);
+    layout.registerComponent('shell', function( container, state ){
+        container.getElement().html(template);
+    });
+    layout.init();
+    boss.layout = layout;
+
+    var vm = new Vue({
         el: '.shell'
     });
 
