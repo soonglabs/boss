22296d7208f7505900765c75003b06ea31228d30
Scott Russell <scrussell24@gmail.com>
2016-11-20 15:35:00 -0600

    add http cmd

---
+++ src/root/cmd/help/src/index.js
@@ -0,0 +1,29 @@
+(function(args, client){
+    boss.lib.print.log("[[;Orange;]Shells Commands:]", client);
+    boss.lib.print.log("[[;Yellow;]Navigation:]", client);
+    boss.lib.print.log("\tcd", client);
+    boss.lib.print.log("\tcp", client);
+    boss.lib.print.log("\tmv", client);
+    boss.lib.print.log("\tls", client);
+    boss.lib.print.log("\tmkdir", client);
+    boss.lib.print.log("\twrite", client);
+    boss.lib.print.log("\tread", client);
+    boss.lib.print.log("\trm", client);
+
+    boss.lib.print.log("[[;Yellow;]User:]", client);
+    boss.lib.print.log("\tadduser", client);
+    boss.lib.print.log("\trmuser", client);
+
+    boss.lib.print.log("[[;Orange;]Apps:]", client);
+    boss.lib.print.log("\tshell", client);
+    boss.lib.print.log("\tedit", client);
+    boss.lib.print.log("\techo", client);
+    boss.lib.print.log("\thttp", client);
+    boss.lib.print.log("\tjs", client);
+
+    boss.lib.print.log("[[;Orange;]System:]", client);
+    boss.lib.print.log("\tabout", client);
+    boss.lib.print.log("\texport", client);
+    boss.lib.print.log("\texit", client);
+    boss.lib.print.log("\thelp", client);
+});
