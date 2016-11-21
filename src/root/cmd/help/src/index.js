3690378088ab9d669fdcd8ad01a266acc4d739ec
Scott Russell <scrussell24@gmail.com>
2016-11-20 22:07:41 -0600

    add pad function

--- src/root/cmd/help/src/index.js
+++ src/root/cmd/help/src/index.js
@@ -1,18 +1,26 @@
 (function(args, client){
+
+    var pad = function (string, pad, char) {
+        while(string.length < pad){
+            string += char;
+        }
+        return string;
+    };
+
     boss.lib.print.log("[[;Orange;]Shells Commands:]", client);
     boss.lib.print.log("[[;Yellow;]Navigation:]", client);
-    boss.lib.print.log("\tcd", client);
-    boss.lib.print.log("\tcp", client);
-    boss.lib.print.log("\tmv", client);
-    boss.lib.print.log("\tls", client);
-    boss.lib.print.log("\tmkdir", client);
-    boss.lib.print.log("\twrite", client);
-    boss.lib.print.log("\tread", client);
-    boss.lib.print.log("\trm", client);
+    boss.lib.print.log(pad('\tcd ', 15, ' ') + "Change Directory", client);
+    boss.lib.print.log(pad('\tcp ', 15, ' ') + "Copy", client);
+    boss.lib.print.log(pad('\tmv ', 15, ' ') + "Move", client);
+    boss.lib.print.log(pad('\tls ', 15, ' ') + "List", client);
+    boss.lib.print.log(pad('\tmkdir ', 15, ' ') + "Make Directory", client);
+    boss.lib.print.log(pad('\twrite ', 15, ' ') + "Write to File", client);
+    boss.lib.print.log(pad('\tread ', 15, ' ') + "Read File", client);
+    boss.lib.print.log(pad('\trm ', 15, ' ') + "Remove", client);
 
     boss.lib.print.log("[[;Yellow;]User:]", client);
-    boss.lib.print.log("\tadduser", client);
-    boss.lib.print.log("\trmuser", client);
+    boss.lib.print.log(pad('\tadduser ', 15, ' ') + 'Create User', client);
+    boss.lib.print.log(pad('\trmuser ', 15, ' ') + 'Remove User', client);
 
     boss.lib.print.log("[[;Orange;]Apps:]", client);
     boss.lib.print.log("\tshell", client);
