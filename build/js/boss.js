1293b2d3d5379f2c3a8bde3382cfdb5135434f3d
Scott Russell <scrussell24@gmail.com>
2016-11-26 09:25:29 -0600

    commit build dir

---
+++ build/js/boss.js
@@ -0,0 +1,69 @@
+'use strict'
+
+function Boss(root, name){
+
+    var boss = this;
+
+    var loadFileSystem = root => {
+        this.FileSystem = eval(root.dirs.sys.dirs.FileSystem.dirs.src.files['index.js'].data);
+    }
+
+    var loadJS = (folder, name) => {
+        var dirs = this.fs.get_dirs('/' + folder);
+        for(var dir in dirs){
+            var code = this.fs.get_file('/' + folder + '/' + dirs[dir]  + '/src', 'index.js');
+            this[name][dirs[dir]] = eval(code);
+         }
+    }
+
+    var loadApp = (folder, name) => {
+        var dirs = this.fs.get_dirs('/' + folder);
+        for(var dir in dirs){
+            var code = this.fs.get_file('/' + folder + '/' + dirs[dir]  + '/src', 'index.js');
+            this[name][dirs[dir]] = eval(code);
+         }
+    }
+
+    var loadLibraries = () => {
+        loadJS('lib', 'lib');
+    }
+
+    var loadCommands = () => {
+        loadJS('cmd', 'cmd');
+    }
+
+    var loadApps = () => {
+        loadApp('app', 'app');
+    }
+
+    var runInit = () => {
+        var files = this.fs.get_files('/etc');
+        for(var file in files){
+            var code = this.fs.get_file('/etc', files[file]);
+            var fn = eval(code);
+            fn();
+        }
+    }
+
+    this.reload = function(){
+        this.cmd = {};
+        this.lib = {};
+        this.app = {};
+        loadLibraries();
+        loadCommands();
+        loadApps(); //Should this be at runtime?
+        runInit();
+    }
+
+    loadFileSystem(root);
+    this.fs = new this.FileSystem(root, name);
+    this.reload();
+    this.interpreters = [new this.lib.Login(this).username];
+}
+
+try{
+    module.exports = Boss;
+} catch(err){
+    //do nothing, assuming the file is being run in a browser
+}
+
