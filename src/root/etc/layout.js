0fb78aee432403634bcf8528d9b5f213be9ee259
Scott Russell <scrussell24@gmail.com>
2016-12-08 21:43:49 -0600

    add goldenlayout options as comment

--- src/root/etc/layout.js
+++ src/root/etc/layout.js
@@ -1,6 +1,32 @@
 (function(){
     'use strict';
     var layoutConfig = {
+        // settings:{
+        //     hasHeaders: true,
+        //     constrainDragToContainer: true,
+        //     reorderEnabled: true,
+        //     selectionEnabled: false,
+        //     popoutWholeStack: false,
+        //     blockedPopoutsThrowError: true,
+        //     closePopoutsOnUnload: true,
+        //     showPopoutIcon: true,
+        //     showMaximiseIcon: true,
+        //     showCloseIcon: true
+        // },
+        // dimensions: {
+        //     borderWidth: 5,
+        //     minItemHeight: 10,
+        //     minItemWidth: 10,
+        //     headerHeight: 20,
+        //     dragProxyWidth: 300,
+        //     dragProxyHeight: 200
+        // },
+        // labels: {
+        //     close: 'close',
+        //     maximise: 'maximise',
+        //     minimise: 'minimise',
+        //     popout: 'open in new window'
+        // },
         content: [{
             type: 'row',
             content: []
