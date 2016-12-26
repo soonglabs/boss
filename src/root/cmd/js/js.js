4e7ac17ac4927535e5688f58b486eb436bcfd8d4
Scott Russell <scrussell24@gmail.com>
2016-12-25 22:42:37 -0600

    update js cmd

--- src/root/cmd/js/js.js
+++ src/root/cmd/js/js.js
@@ -1,16 +1,9 @@
 (function(args, client){
-
     if(args.length > 1){
-        var result = boss.lib.utils.splitPathFilename(args[1], client);
-        var path = result.path;
-        var name = result.name;
-    }if(name){
         try{
-            boss.lib.print.log(eval(boss.fs.get_file(path, name)), client);
+            boss.lib.print.log(eval(args[1]), client);
         } catch(err){
             boss.lib.print.error('error : ' + err, client);
         }
-    } else if(!name) {
-        boss.lib.print.error('error: no file argument', client);
     }
-})
+});
