4859c767bd14b4f31b626cb83192aec8f5813edd
Scott Russell <scrussell24@gmail.com>
2017-01-07 17:24:17 -0600

    added picnic css

--- src/root/etc/component.js
+++ src/root/etc/component.js
@@ -1,20 +1,21 @@
 (function(){
     'use strict';
-    //this binds the boss component to #boss
+    //this binds the boss component to #boss element
     //e.g. <div id="boss"></div>
-    //we also register boss sub components here
+    //why do we do this? So the user doesn't need
+    //to know anything about Vuejs and all view
+    //code and templates are in the image
 
-    //register header component
-    Vue.component('boss-hd', {
-        template: `<div id="header">
-                        <span>&#931 boss</span>
-                  </div>`
-    });
 
     //register boss component
     Vue.component('boss', {
         template: `<div>
-                    <boss-hd></boss-hd>
+                    <div>
+                        <nav class="nav">
+                            <a href="#"><span>&#931</span></a>
+                            <a href="#"><span>Apps</span></a>
+                        </nav>
+                    </div>
                     <div id="glayout"></div>
                     <div id="logo">&#931</div>
                   </div>`
