7a929e7494887fd25b0060774dc1743121149be8
Scott Russell <scrussell24@gmail.com>
2017-03-19 20:58:41 -0500

    update todo

--- src/root/app/todo/components.js
+++ src/root/app/todo/components.js
@@ -7,8 +7,7 @@ Vue.component('todo-app', {
                         <div class="add-new-title">Add Todo</div>
                         <form>
                             <div class="form-group container">
-                                <input v-model="newTodo.title" placeholder="title"></input>
-                                <input v-model="newTodo.task" placeholder="description" style="width"></input>
+                                <textarea v-model="newTodo.text" placeholder="text" cols=50 rows=3></textarea>
                                 <button
                                     class="btn dark-b"
                                     v-on:click="addTodo()"
@@ -23,31 +22,28 @@ Vue.component('todo-app', {
                     <div class="todos-title">TODOS</div>
                     <ul id="todos">
                         <li v-for="todo in todos">
-                            <span class="todo-title">{{ todo.title }}</span>
-                            <div class="btn-group">
+                            <pre class="todo-title">{{ todo.text }}</pre>
+                            <div class="btn-group" style="float:left;">
                                 <button 
-                                    class="btn light-b" 
-                                    v-on:click="removeTodo(todo)" 
+                                    class="btn light-b"
+                                    v-on:click="removeTodo(todo)"
                                     title="remove todo">
                                     [<i class="fa fa-check" aria-hidden="true"></i>|
                                 </button>
                                 <button
-                                    class="btn light-b" 
-                                    v-on:click="editTodo(todo)" 
+                                    class="btn light-b"
+                                    v-on:click="editTodo(todo)"
                                     title="edit todo">
                                     <i class="fa fa-pencil" aria-hidden="true"></i>]
-                                    </i>
                                 </button>
                             </div>
-                            <span class="todo-task">{{todo.task}}</span>
                             <div v-if="todo.editing" class="edit-container dark-shadow">
                                 <div class="add-new">
                                     <div class="add-new-title">Edit Todo</div>
                                     <form>
                                         <div class="form-group container">
-                                            <input v-model="todo.title" placeholder="title"></input>
-                                            <input v-model="todo.task" placeholder="description" style="width"></input>
-                                            <button 
+                                            <textarea v-model="todo.text" placeholder="text" cols=50 rows=5></textarea>
+                                           <button 
                                                 class="btn blue-b" 
                                                 v-on:click="closeEdit(todo)" 
                                                 title="edit todo">
@@ -64,12 +60,10 @@ Vue.component('todo-app', {
     methods: {
         addTodo: function(){
             this.todos.push({
-                title: this.newTodo.title,
-                task: this.newTodo.task,
+                text: this.newTodo.text,
                 editing: false
             });
-            this.newTodo.title = 'title';
-            this.newTodo.task = 'description';
+            this.newTodo.text = 'text';
             this.save();
         },
         removeTodo: function(todo){
@@ -95,8 +89,7 @@ Vue.component('todo-app', {
         return {
             todos: [],
             newTodo: {
-                title: 'title',
-                task: 'description'
+                text: 'text'
             }
         }
     },
